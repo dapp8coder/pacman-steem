abfdeb7e9b05b30323a253a0e87b10dc0c47aec0
Andrew Chaney (netuoso) <andrewc@pobox.com>
2017-12-19 22:16:35 -0600

    blacklisters

--- config.js
+++ config.js
@@ -3,6 +3,14 @@ var steemAcct = {username: 'steemretro', activeKey: '55'};
 var blacklistUsers = [
 	'kwonn',
 	'truthism',
+	'enisshkurti',
+	'johncarmack',
+	'sydneyshow',
+	'rica170',
+	'qweku',
+	'annaburska',
+	'beautyphotos',
+	'phileas0408'
 ];
 
 module.exports = {
